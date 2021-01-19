db061b5281a73fd4773cae752ec4fa1f9a8b9dda
Joshua Mba <angelicjoekeyz@gmail.com>
2021-01-19 16:44:59 +0100

    Feat: initial project setup

---
+++ src/controllers/course.controller.js
@@ -0,0 +1,34 @@
+const CourseServ = require("./../services/course.service");
+ 
+const response = require("./../utils/response");
+
+class CourseContoller {
+
+  async create(req, res) {
+    const result = await CourseServ.create(req.body);
+    res.status(201).send(response("course created", result));
+  } 
+
+  async getAll(req, res) {
+    const result = await CourseServ.getAll();
+    res.status(200).send(response("All course", result));
+  }
+
+  async getOne(req, res) {
+    const result = await CourseServ.getOne(req.params.courseId);
+    res.status(200).send(response("course data", result));
+  }
+
+  async update(req, res) {
+    const result = await CourseServ.update(req.params.courseId, req.body);
+    res.status(200).send(response("course updated", result));
+  }
+  
+  async delete(req, res) {
+    const result = await CourseServ.delete(req.params.courseId);
+    res.status(200).send(response("course deleted", result));
+  }
+
+}
+
+module.exports = new CourseContoller();
---
+++ src/services/course.service.js
@@ -0,0 +1,41 @@
+const Course = require("./../models/course.model");
+const CustomError = require("./../utils/custom-error");
+
+class CourseService {
+
+  async create(data) {
+    return await new Course(data).save();
+  }
+
+  async getAll() {
+    return await Course.find({});
+  }
+
+  async getOne(courseId) {
+    const course = await Course.findOne({ _id: courseId });
+    if (!course) throw new CustomError("Course does not exists");
+
+    return course
+  }
+
+  async update(courseId, data) {
+    const course = await Course.findByIdAndUpdate(
+    { _id: courseId },
+    { $set: data },
+    { new: true }
+    );
+
+    if (!course) throw new CustomError("Course dosen't exist", 404);
+
+    return course;
+  }
+
+  async delete(courseId) {
+    const course = await Course.findOne({ _id: courseId });
+    course.remove()
+    return course
+  }
+
+}
+
+module.exports = new CourseService();
