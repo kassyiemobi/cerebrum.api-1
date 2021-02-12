33b3688f1aa470c3119c139f7b1ac17c48b048c2
Joshua Mba <angelicjoekeyz@gmail.com>
2021-02-12 08:41:19 +0100

    Update: localhost for payment to firebase

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -132,7 +132,7 @@ class PaymentContoller {
               transaction.save()
                 .then((trans)=> {
                   const payId = JSON.stringify(pay._id)
-                  res.redirect('http://localhost:3000/user/course/payment/success/?id='+pay._id+'&&courseId='+pay.course_id);
+                  res.redirect('http://https://cerebrum-v1.web.app/user/course/payment/success/?id='+pay._id+'&&courseId='+pay.course_id);
                 })
                 .catch((e)=>{
                   console.log(e);
@@ -140,7 +140,7 @@ class PaymentContoller {
             })
             .catch((e)=>{
               console.log(e.response);
-              res.redirect('http://localhost:3000/user/course/payment/fail');
+              res.redirect('http://https://cerebrum-v1.web.app/user/course/payment/fail');
             });
 
           }
